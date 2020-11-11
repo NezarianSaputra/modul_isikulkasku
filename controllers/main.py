189420c0b50a69933c12a9569a3a4ab7e32ae769
NezarianSaputra <61400427+NezarianSaputra@users.noreply.github.com>
2020-11-11 15:00:27 +0700

    Add files via upload

--- controllers/main.py
+++ controllers/main.py
@@ -1,27 +1,154 @@
+import odoo
+
+from odoo import http, models, fields, _
+from odoo.http import request
+import json
+import unicodedata
+import base64
+from odoo.addons.website.controllers.main import QueryURL
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 
+class WebsiteSaleInherit(WebsiteSale):
 
-class Shop(http.Controllers):
-		
-	@http.route('/sale/support', type='http', website=True)
-		def sale_support(self, **kwargs):
-		return "Heloo, World"
-    	# controller ini hanya untuk merender form
-    	# return request.render('tutorial_controller.sale_support')
-    
-    # @http.route(['/shop/cart/update'], type='http', auth="public", methods=['POST'], website=True, csrf=False)
-    # def cart_update(self, product_id, add_qty=1, set_qty=0, **kw):
-    #     sale_order = request.website.sale_get_order(force_create=True)
-    #     if sale_order.state != 'draft':
-    #         request.session['sale_order_id'] = None
-    #         sale_order = request.website.sale_get_order(force_create=True)
-    #     sale_order._cart_update(
-    #         product_id=int(product_id),
-    #         add_qty=add_qty,
-    #         set_qty=set_qty,
-    #         attributes=self._filter_attributes(**kw),
-    #     )
-    #     return request.redirect("/shop/cart")
+    @http.route([
+        '/shop',
+        '/shop/page/<int:page>',
+        '/shop/category/<model("product.public.category"):category>',
+        '/shop/category/<model("product.public.category"):category>/page/<int:page>'
+    ], type='http', auth="public", website=True)
+    def shop(self, page=0, category=None, search='', ppg=False, **post):
+        if ppg:
+            try:
+                ppg = int(ppg)
+            except ValueError:
+                ppg = PPG
+            post["ppg"] = ppg
+        else:
+            ppg = PPG
 
-    # def _filter_attributes(self, **kw):
-    #     return {k: v for k, v in kw.items() if "attribute" in k}
+        if category:
+            category = request.env['product.public.category'].search([('id', '=', int(category))], limit=1)
+            if not category:
+                raise NotFound()
+
+        attrib_list = request.httprequest.args.getlist('attrib')
+        attrib_values = [map(int, v.split("-")) for v in attrib_list if v]
+        attributes_ids = set([v[0] for v in attrib_values])
+        attrib_set = set([v[1] for v in attrib_values])
+
+        domain = self._get_search_domain(search, category, attrib_values)
+
+        keep = QueryURL('/shop', category=category and int(category), search=search, attrib=attrib_list, order=post.get('order'))
+        pricelist_context = dict(request.env.context)
+        if not pricelist_context.get('pricelist'):
+            pricelist = request.website.get_current_pricelist()
+            pricelist_context['pricelist'] = pricelist.id
+        else:
+            pricelist = request.env['product.pricelist'].browse(pricelist_context['pricelist'])
+
+        request.context = dict(request.context, pricelist=pricelist.id, partner=request.env.user.partner_id)
+
+        url = "/shop"
+        if search:
+            post["search"] = search
+        if attrib_list:
+            post['attrib'] = attrib_list
+
+        categs = request.env['product.public.category'].search([('parent_id', '=', False)])
+        Product = request.env['product.template']
+
+        parent_category_ids = []
+        if category:
+            url = "/shop/category/%s" % slug(category)
+            parent_category_ids = [category.id]
+            current_category = category
+            while current_category.parent_id:
+                parent_category_ids.append(current_category.parent_id.id)
+                current_category = current_category.parent_id
+
+        product_count = Product.search_count(domain)
+        pager = request.website.pager(url=url, total=product_count, page=page, step=ppg, scope=7, url_args=post)
+        products = Product.search(domain, limit=ppg, offset=pager['offset'], order=self._get_search_order(post))
+
+        ProductAttribute = request.env['product.attribute']
+        if products:
+            # get all products without limit
+            selected_products = Product.search(domain, limit=False)
+            attributes = ProductAttribute.search([('attribute_line_ids.product_tmpl_id', 'in', selected_products.ids)])
+        else:
+            attributes = ProductAttribute.browse(attributes_ids)
+
+        from_currency = request.env.user.company_id.currency_id
+        to_currency = pricelist.currency_id
+        compute_currency = lambda price: from_currency.compute(price, to_currency)
+
+        values = {
+            'search': search,
+            'category': category,
+            'attrib_values': attrib_values,
+            'attrib_set': attrib_set,
+            'pager': pager,
+            'pricelist': pricelist,
+            'products': products,
+            'search_count': product_count,  # common for all searchbox
+            'bins': TableCompute().process(products, ppg),
+            'rows': PPR,
+            'categories': categs,
+            'attributes': attributes,
+            'compute_currency': compute_currency,
+            'keep': keep,
+            'parent_category_ids': parent_category_ids,
+        }
+        if category:
+            values['main_object'] = category
+        return request.render("website_sale.products", values)
+
+    @http.route(['/shop/product/<model("product.template"):product>'], type='http', auth="public", website=True)
+    def product(self, product, category='', search='', **kwargs):
+        if not product.can_access_from_current_website():
+            raise NotFound()
+
+        product_context = dict(request.env.context,
+                               active_id=product.id,
+                               partner=request.env.user.partner_id)
+        ProductCategory = request.env['product.public.category']
+
+        if category:
+            category = ProductCategory.browse(int(category)).exists()
+
+        attrib_list = request.httprequest.args.getlist('attrib')
+        attrib_values = [[int(x) for x in v.split("-")] for v in attrib_list if v]
+        attrib_set = {v[1] for v in attrib_values}
+
+        keep = QueryURL('/shop', category=category and category.id, search=search, attrib=attrib_list)
+
+        categs = ProductCategory.search([('parent_id', '=', False)])
+
+        pricelist = request.website.get_current_pricelist()
+
+        from_currency = request.env.user.company_id.currency_id
+        to_currency = pricelist.currency_id
+        compute_currency = lambda price: from_currency._convert(price, to_currency, request.env.user.company_id, fields.Date.today())
+
+        if not product_context.get('pricelist'):
+            product_context['pricelist'] = pricelist.id
+            product = product.with_context(product_context)
+
+        values = {
+            'search': search,
+            'category': category,
+            'pricelist': pricelist,
+            'attrib_values': attrib_values,
+            'compute_currency': compute_currency,
+            'attrib_set': attrib_set,
+            'keep': keep,
+            'categories': categs,
+            'main_object': product,
+            'product': product,
+            'optional_product_ids': [p.with_context({'active_id': p.id}) for p in product.optional_product_ids],
+            'get_attribute_exclusions': self._get_attribute_exclusions,
+            'available_qty': product.sudo().qty_available, # tambahan tidak ada di source code asli odoo
+            'product_uom_name': product.sudo().uom_id.display_name # tambahan tidak ada di source code asli odoo
+        }
+        return request.render("website_sale.product", values)
+        
